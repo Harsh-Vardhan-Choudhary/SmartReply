0edc6b2b5a49548d004f5aabcd2e8f5711077cd2
harsh-vardhan-choudhary <choudharyharsh964@gmail.com>
2025-03-03 16:23:33 +0530

    first commit

---
+++ src/main/java/com/smart_reply/SmartReplyApplication.java
@@ -0,0 +1,13 @@
+package com.smart_reply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartReplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartReplyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smart_reply/SmartReplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_reply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartReplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
